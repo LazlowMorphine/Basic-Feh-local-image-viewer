6c0c435ab285e86190b9039497a0e337479cc842
C. Darcy <66063746+LazlowMorphine@users.noreply.github.com>
2021-01-22 13:14:06 +1100

    Update start-picture-frame.sh

--- start-picture-frame.sh
+++ start-picture-frame.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority /usr/bin/feh -q -p -Z -F -R 60 -Y -D 15.0 /home/pi/Pictures
+DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority /usr/bin/feh -q -p -Z -F -R --auto-rotate 60 -Y -D 15.0 /home/pi/Pictures
